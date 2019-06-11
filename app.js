5ebf090b99e23daf413ab63cbbeb0031b8f6b8cc
messickr <rnmessick@gmail.com>
2019-06-11 14:43:10 -0700

    Updated objects as constructors, built out table skeleton

--- app.js
+++ app.js
@@ -1,71 +1,38 @@
-'use strict';
+// 'use strict';
 
-var hours = ['6am: ', '7am: ', '8am: ','9am: ','10am: ','11am: ','12am: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ', '8pm: '];
-var cookieStand1 = {
-  location: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  averageCookieSale: 6.3,
-  averageCustomerArr: [],
-  cookiesEachHourArr: [],
-  totalCookies: 0
-  
-};
+//building constructor
+var allCookieStoresArray = [];
+function CookieStore (location, minCustomer, maxCustomer, averageCookieSale){
+  this.location = location;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.averageCookieSale = averageCookieSale;
+}
 
-cookieStand1.hourlyCustomers = function(){
-  for(var i=0; i <hours.length; i++){
-    var customerPerHour = Math.floor (Math.random() * (this.maxCust - this.minCust) + minCust) + this.averageCustomerArr.push(customerPerHour);
-  }
-},
-cookieStand1.cookiesSoldPerCust = function(){
-  this.hourlyCustomers();
-  for(var i=0; i >hours.length; i++){
-    var cookiesPerHour = Math.ceil(this.averageCustomerArr[i] * this.averageCookieSale);
-    this.totalCookies += cookiesPerHour;
-  }
-},
+// CookieStore.prototype.calculateCustomersPerHour = function(){
+//   var customers = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
 
-cookieStand1.salesPage = function(){
-  var hourlyList = document.getElementById('hourlyListCookieStand1');
-  this.cookiesSoldPerCust();
-  for(var i=0; i < this.cookiesEachHourArr.length; i++){
-    var listElement = document.createElement('li');
-    listElement.textContent = hours[i] + this.cookiesEachHourArr[i] + ' cookies';
-    cookieStand1.appendChild(listElement);
-  }
- 
-};
+//   return customers;
+// };
 
-// var cookieStand2 = {
-//   location: 'SeaTac Airport',
-//   minCust: 3,
-//   maxCust: 24,
-//   averageCookieSale: 1.2
-// }
+// CookieStore.prototype.calculateCookiesSoldInOneHour = function(){
+//   //multiply an amount of customers by the average amount of cookies
+//   var customersInAnHour = this.calculateCustomersPerHour();
+//   var cookies = this.averageCookieSale * customersInAnHour;
+//   return cookies;
+// };
 
-// cookieStand3 = {
-//   location: 'Seattle Center',
-//   minCust: 11,
-//   maxCust: 38,
-//   averageCookieSale: 3.7
-// }
+// CookieStore.prototype.calculateTotalsPerHour = function(){
+//   for(var i = 0; i < 15; i++){
+//     var totalInOneHour = this.calculateCookiesSoldInOneHour();
+//     this.totalsPerHour.push(totalInOneHour);
+//     total +=
 
-// cookieStand4 = {
-//   location: 'Capitol Hill',
-//   minCust: 20,
-//   maxCust: 38,
-//   averageCookieSale: 2.3
-// }
-
-// cookieStand5 = {
-//   location: 'Alki',
-//   minCust: 2,
-//   maxCust: 16,
-//   averageCookieSale: 4.6,
-//   hourlyCookieSale: function(){
-//     var cookiesByHour = ((this.minCust * this.maxCust)/2);
 //   }
-// }
 
-
+allCookieStoresArray.push(new CookieStore('1st and Pike', 23, 65, 6.3));
+allCookieStoresArray.push(new CookieStore('SeaTac Airport', 3, 24, 1.2));
+allCookieStoresArray.push(new CookieStore('Seattle Center', 11, 38, 3.7));
+allCookieStoresArray.push(new CookieStore('Capitol Hill', 20, 38, 2.3));
+allCookieStoresArray.push(new CookieStore('Alki', 2, 16, 4.6));
 
