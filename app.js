be2665dbeac1ad0f53ffabf0dda2ecc03066cd34
Matt Stuhring <whereyouatmatt@gmail.com>
2019-06-12 18:00:01 -0700

    Implemented form and form constructor

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 //building constructor
 var allCookieStoresArray = [];
 
-var storeUlEl = document.getElementById('store-container');
+var hours = ['6am', '7am', '8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm', '8pm'];
+
+var storeTableEl = document.getElementById('store-container');
 
 function CookieStore (location, minCustomer, maxCustomer, averageCookieSale){
   this.location = location;
@@ -18,7 +20,7 @@ function CookieStore (location, minCustomer, maxCustomer, averageCookieSale){
 
 CookieStore.prototype.calculateCustomersPerHour = function(){
   var customers = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer;
-  console.log('calculateCustomerPerHour ' + customers);
+  // console.log('calculateCustomerPerHour ' + customers);
   return customers; 
 };
 
@@ -26,7 +28,7 @@ CookieStore.prototype.calculateCookiesSoldInOneHour = function(){
   //multiply an amount of customers by the average amount of cookies
   var customersInAnHour = this.calculateCustomersPerHour();
   var cookies = Math.floor(this.averageCookieSale * customersInAnHour);
-  console.log('calculateCookiesSoldInOneHour' + cookies);
+  // console.log('calculateCookiesSoldInOneHour' + cookies);
   return cookies;
 };
 
@@ -38,32 +40,8 @@ CookieStore.prototype.calculateTotalsPerHour = function(){
     total += totalInOneHour;
   }
   this. total = total;
-  console.log('calculateTotalsPerHour ' + total);
+  // console.log('calculateTotalsPerHour ' + total);
 };
-
-CookieStore.prototype.render = function() {
-  var trEl = document.createElement('tr');
-  trEl.textContent = this.location;
-  storeUlEl.appendChild(trEl);
-
-  for(var i = 0; i < this.cookiePerHourPerStoreArray.length; i++) {
-    var tdEl = document.createElement('td');
-    tdEl.textContent = this.cookiePerHourPerStoreArray[i];
-    trEl.appendChild(tdEl);
-  }
-  storeUlEl.appendChild(trEl);
-};
-//1. reference the parent
-var trEl = document.createElement('tr');
-
-//2. create element
-var totalTdEl = document.createElement('td');
-// 3. fill new element with content
-totalTdEl.textContent = 'Total: ' + this.total;
-trEl.appendChild(totalTdEl);
-//4.append to parents
-storeUlEl.appendChild(trEl);
-
 
 //creating my new store objects
 allCookieStoresArray.push(new CookieStore('1st and Pike', 23, 65, 6.3));
@@ -72,4 +50,103 @@ allCookieStoresArray.push(new CookieStore('Seattle Center', 11, 38, 3.7));
 allCookieStoresArray.push(new CookieStore('Capitol Hill', 20, 38, 2.3));
 allCookieStoresArray.push(new CookieStore('Alki', 2, 16, 4.6));
 
-console.log('My stores ', allCookieStoresArray);
+// Call to render the table elements
+var renderTable = function() {
+  // Create a table row
+  var trEl = document.createElement('tr');
+  storeTableEl.appendChild(trEl);
+
+  // Create table header "Stores"
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Stores';
+  trEl.appendChild(thEl);
+
+  // Iterate through store hours and create new table headers. ex 6am -8pm
+  for (var k = 0; k < hours.length; k++) {
+    thEl = document.createElement('th');
+    thEl.textContent = hours[k];
+    trEl.appendChild(thEl);
+  }
+
+  // Create final column with "Daily Totals"
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Totals';
+  trEl.appendChild(thEl);
+
+  // Create a new table row
+  trEl = document.createElement('tr');
+  storeTableEl.appendChild(trEl);
+
+  // Iterate through cookies store array
+  for(var i = 0; i < allCookieStoresArray.length; i++) {
+    // Create table data element with location name
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allCookieStoresArray[i].location;
+    trEl.appendChild(tdEl);
+
+    // Iterated through total cookies per hour for each store
+    for (var j = 0; j < allCookieStoresArray[i].totalsPerHour.length; j++) {
+      // Create new table data element for each data point
+      tdEl = document.createElement('td');
+      tdEl.textContent = allCookieStoresArray[i].totalsPerHour[j];
+      trEl.appendChild(tdEl);
+
+      // Check if we are at the end of the store array; if so create a new table row
+      if (allCookieStoresArray[i].totalsPerHour.length - 1 === j) {
+        // Create table data with final total for each store
+        tdEl = document.createElement('td');
+        tdEl.textContent = allCookieStoresArray[i].total;
+        trEl.appendChild(tdEl);
+
+        // Create a new table row the next store
+        trEl = document.createElement('tr');
+        storeTableEl.appendChild(trEl);
+        break;
+      }
+    }
+
+  }
+
+};
+
+var CookieForm = function(location, min, max, avg) {
+  this.location = location;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+};
+
+var form = document.getElementById('storeForm');
+
+var handleFormSubmit = function(formSubmitEvent) {
+  formSubmitEvent.preventDefault();
+
+  var location = formSubmitEvent.target.location.value;
+  var min = parseInt(formSubmitEvent.target.min.value);
+  var max = parseInt(formSubmitEvent.target.max.value);
+  var avg = parseInt(formSubmitEvent.target.avg.value);
+
+  console.log(location);
+  console.log(min);
+  console.log(max);
+  console.log(avg);
+
+
+  var newCookieStore = new CookieForm(location, min, max, avg);
+
+  console.log(newCookieStore);
+
+  allCookieStoresArray.push(new CookieStore(newCookieStore.location, newCookieStore.min, newCookieStore.max, newCookieStore.avg));
+  // allCookieStoresArray.push(newCookieStore);
+
+  console.log(allCookieStoresArray);
+
+  storeTableEl.innerHTML = '';
+
+  renderTable();
+
+};
+
+form.addEventListener('submit', handleFormSubmit);
+
+renderTable();
